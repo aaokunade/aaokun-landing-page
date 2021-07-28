ed21c7e34d2e2b91887a9bd59613514b2d443707
aaokunade <67062110+aaokunade@users.noreply.github.com>
2021-07-28 17:18:53 +0100

    paths added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React from "react";
+import Home from "./pages/home";
+import Blog from "./pages/blog";
+import Project from "./pages/project";
+import Contact from "./pages/contact";
+import About from "./pages/about";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const App = () => (
+	<div>
+    <Router>
+	 <Switch>
+	 	<Route path="/contacts"><Contact /></Route>
+		 <Route path="/blog"><Blog /></Route>
+		 <Route path="/" exact><Home /></Route>
+		 <Route path="/about"><About /></Route>
+		 <Route path="/project"><Project /></Route>
+	    </Switch>
+    </Router>
+	</div>	
+);
 
 export default App;
